7d5d0195d18b132c40e04a7224cd997b34f2061f
Jonathan Palafoutas <jpalafou@princeton.edu>
2023-12-12 17:44:06 -0500

    add: day 6 part 2

--- day06/race.py
+++ day06/race.py
@@ -18,28 +18,31 @@ def boat_strategies(time: int) -> np.ndarray:
     returns:
         out     pairs of time spent charging and the resulting distance
     """
-    list_of_outcomes = []
-    for charge_time in range(time):
-        sail_time = time - charge_time
-        distance = sail_time * charge_time
-        list_of_outcomes.append((charge_time, distance))
-    out = np.asarray(list_of_outcomes)
+    charge_times = np.arange(time)[:, np.newaxis]
+    sail_times = time - charge_times
+    distances = sail_times * charge_times
+    out = np.hstack((charge_times, distances))
     return out
 
 
 # read race data
-with open("inputs.txt") as file:
-    rows = file.readlines()
-    data = []
-    for row, label in zip(rows, ["Time:", "Distance:"]):
-        data_string = row.replace(label, "").strip()
-        data.append([int(i) for i in _remove_empty(data_string.split(" "))])
-    times, records = data
+for multiple_races in [True, False]:
+    with open("inputs.txt") as file:
+        rows = file.readlines()
+        data = []
+        for row, label in zip(rows, ["Time:", "Distance:"]):
+            stripped_row = row.replace(label, "").strip()
+            if multiple_races:
+                data.append([int(i) for i in _remove_empty(stripped_row.split(" "))])
+            else:
+                data.append([int(stripped_row.replace(" ", ""))])
 
-# count the number of ways to beat the record for each race
-winning_strategy_product = 1
-for time, record in zip(times, records):
-    outcomes = boat_strategies(time)
-    winning_stratgies = np.sum(np.where(outcomes[:, 1] > record, 1, 0))
-    winning_strategy_product *= winning_stratgies
-print(f"Product of winning race strategies: {winning_strategy_product}")
+    # count the number of ways to beat the record for each race
+    times, records = data
+    winning_strategy_product = 1
+    for time, record in zip(times, records):
+        outcomes = boat_strategies(time)
+        winning_stratgies = np.sum(np.where(outcomes[:, 1] > record, 1, 0))
+        winning_strategy_product *= winning_stratgies
+    print(f"{multiple_races=}")
+    print(f"\tProduct of winning race strategies: {winning_strategy_product}")
