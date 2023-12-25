0b0d27b875044a20b7cb6033e993571ad5686821
Jonathan Palafoutas <jpalafou@princeton.edu>
2023-12-25 16:36:42 -0600

    fix: better dynamic programming

--- day12/spring.py
+++ day12/spring.py
@@ -1,72 +1,40 @@
 # I had to look at other people's solutions in order to write one for Part 2.
 # I especially followed the approach of fuglede
 # (https://github.com/fuglede/adventofcode/blob/master/2023/day12/solutions.py)
+# and jonathanpaulson
+# (https://github.com/jonathanpaulson/AdventOfCode/blob/master/2023/12.py)
 
 from functools import cache
-# import numpy as np
-import cProfile
+
 
 @cache
-def count_possible(record: str, groups: tuple, num_possible: int = 0) -> int:
+def count_possible(record: str, groups: tuple, hash_counter: int = 0) -> int:
     """
     args:
         record          arbitrary series of ".", "#", and "?"
         groups          number of contiguous strings of "#" to form
-        num_possible    number of configurations of the "?" characters in record which
-                        result in contiguous strings of "#" matching groups
-                        initialized as 0 and then increases recursively
+        hash_counter    how many hashtags have we seen in a row?
     returns:
-        num_possible
+        out             number of configurations of the "?" characters in record which
+                        result in contiguous strings of "#" matching groups
     """
-    # print(record, groups)
-    if sum(groups) - record.count("#") > record.count("?"):
-        # invalid case
-        return num_possible
-    # scan with kernel with length of leftmost group (+1 for ".")
-    k = groups[0]
-    prev_char = None
-    for i in range(len(record) - k):
-        scan = record[i : i + k + 1]
-        ambiguous_start = prev_char is None or prev_char in ["?", "."]
-        group_can_be_formed = "." not in scan[:-1]
-        ambiguous_end = scan[-1] in ["?", "."]
-        if ambiguous_start and group_can_be_formed and ambiguous_end:
-            shorter_record = record[i + k + 1 :]
-            if len(groups) > 1:
-                # if the leftmost group could be formed, try forming subsequent groups
-                num_possible = count_possible(shorter_record, groups[1:], num_possible)
-            else:
-                # no subseqeuent groups, so this must be a possible configuration
-                # print("\t", scan)
-                if "#" not in shorter_record:
-                    num_possible += 1
-        if scan[0] == "#":
-            # starting with a # implies this group cannot be any further
-            break
-    return num_possible
+    out = 0
+    if not groups and not hash_counter and "#" not in record:
+        return 1
+    if not record or not groups:
+        return 0
+    if record[0] in [".", "?"]:
+        if hash_counter == groups[0]:
+            out += count_possible(record[1:], groups[1:])
+        elif hash_counter == 0:
+            out += count_possible(record[1:], groups)
+    if record[0] in ["#", "?"]:
+        out += count_possible(record[1:], groups, hash_counter + 1)
+    return out
 
-
-
-# data = [s.split(" ") for s in open("test.txt").read().split("\n")]
-# data = [(r, tuple(int(i) for i in g.split(","))) for r, g in data]
-# total = 0
-# for r, g in data:
-#     num_possible = count_possible("?".join([r] * 1) + ".", g * 1)
-#     total += num_possible
-#     if total < 100:
-#         print(num_possible)
-
-
-# print(count_possible("?#####??#?????.?#.." + ".", (9,2,1)))
-
-# p
 
 for rep in [1, 5]:
-    data = [s.split(" ") for s in open("test.txt").read().split("\n")]
+    data = [s.split(" ") for s in open("inputs.txt").read().split("\n")]
     data = [(r, tuple(int(i) for i in g.split(","))) for r, g in data]
-    counter = 0
-    for i, row in enumerate(data):
-        print(f"{i + 1}/{len(data)}")
-        record, group = row
-        counter += count_possible("?".join([record] * rep) + ".", group * rep)
-    print(counter)
+    count = sum(count_possible("?".join([r] * rep) + ".", g * rep) for r, g in data)
+    print(f"Sum of possible configs ({rep=}): {count}")
